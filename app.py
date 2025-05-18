54a20ceb74c5ced54eaf7ff109c3cdbafe41e42c
Clinton <clinton.pillay@outlook.com>
2025-05-18 21:38:14 +0200

    Everything working

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ from confluent_kafka import Producer
 import json
 import time
 
+
 # Kafka config
 kafka_config = {
     'bootstrap.servers': 'localhost:19092'
 }
 producer = Producer(kafka_config)
+
+
+
+
+master_headers=['id', 'name', 'age', 'email']
+check_header_status = None
+
 
 st.title("Upload CSV and Send Rows to Redpanda Kafka")
 
@@ -16,12 +24,25 @@ uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-    st.write("CSV Preview:")
-    st.dataframe(df)
+    row_headers = df.columns.tolist()
 
-    if st.button("Send to Kafka"):
-        for _, row in df.iterrows():
-            data = row.to_dict()
-            producer.produce('kafka_tut', json.dumps(data).encode('utf-8'))
-        producer.flush()
-        st.success(f" Sent {len(df)} rows to Redpanda Kafka.")
+    if row_headers == master_headers:
+        check_header_status = True
+        st.write(df)
+    else: 
+        check_header_status = None
+        st.write("Please upload a file with the correct format") 
+else:
+    st.write("Please upload a file")
+
+if st.button("Submit", disabled=not check_header_status):
+
+
+    for _, row in df.iterrows():
+        data = row.to_dict()
+        producer.produce('kafka_tut', json.dumps(data).encode('utf-8'))
+    producer.flush()
+    st.success(f" Sent {len(df)} rows to Kafka.")
+
+
+
